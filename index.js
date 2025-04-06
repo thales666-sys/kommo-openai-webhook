4b893a238d43faf18b4e5f396ec16dd19a483000
thales666-sys <thales666@gmail.com>
2025-04-06 20:19:09 -0300

    Teste genérico de webhook

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.post("/", (req, res) => {
-  console.log("✅ Webhook recebido com sucesso!");
-  console.log(req.body); // Mostra o conteúdo do webhook recebido
-  res.status(200).send("OK");
+  console.log("Webhook recebido:", req.body); // Isso vai aparecer no Render
+  res.send("Webhook recebido com sucesso!");
 });
 
 app.get("/", (req, res) => {
-  res.send("Servidor do Webhook está online!");
+  res.send("Servidor do webhook está online!");
 });
 
 app.listen(port, () => {
